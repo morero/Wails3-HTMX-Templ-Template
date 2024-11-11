0921be32d36cf27009cf631aa4ef03330b54ed26
Robin Morero <robin@fabled.se>
2024-11-11 23:31:25 +0100

    Package structure

--- routes/app.go
+++ routes/app.go
@@ -8,8 +8,8 @@ import (
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/go-chi/httplog/v2"
 
-	"components"
-	"pages"
+	"github.com/morero/Wails3-HTMX-Templ-Template/components"
+	"github.com/morero/Wails3-HTMX-Templ-Template/pages"
 )
 
 /*
